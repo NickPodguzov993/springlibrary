c50d721ee0c14cf535112c8a2455f979c6b899b1
NickPodguzov993 <verytallman993@gmail.com>
2024-01-22 17:49:25 +0300

    GH PAGES

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react'
+import { styled } from '@stitches/react'
+import { useTrail, animated } from '@react-spring/web'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled('div', {
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const Container = styled('div', {
+  display: 'flex',
+  gap: 10,
+  marginBottom: 80,
+})
+
+const Box = styled('div', {
+  position: 'relative',
+  height: 50,
+  width: 50,
+})
+
+const SharedStyles = {
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  inset: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'Helvetica',
+  fontWeight: 800,
+  backfaceVisibility: 'hidden',
 }
 
-export default App;
+const FrontBox = styled(animated.div, {
+  ...SharedStyles,
+  backgroundColor: '#fafafa',
+  border: 'solid 2px #1a1a1a',
+})
+
+const BackBox = styled(animated.div, {
+  ...SharedStyles,
+  backgroundColor: '#6cab64',
+  border: 'solid 2px #6cab64',
+  color: '#fafafa',
+})
+
+const items = ['Т', 'Ы', 'П', 'И', 'Д', 'О', 'Р']
+
+export default function App() {
+  const [trail, api] = useTrail(items.length, () => ({
+    rotateX: 0,
+  }))
+
+  const isFlipped = useRef(false)
+
+  const handleClick = () => {
+    if (isFlipped.current) {
+      api.start({
+        rotateX: 0,
+      })
+      isFlipped.current = false
+    } else {
+      api.start({
+        rotateX: 180,
+      })
+      isFlipped.current = true
+    }
+  }
+
+  return (
+      <AppContainer>
+        <Container onClick={handleClick}>
+          {trail.map(({ rotateX }, i) => (
+              <Box key={i}>
+                <FrontBox
+                    key={items[i]}
+                    style={{
+                      transform: rotateX.to(val => `perspective(600px) rotateX(${val}deg)`),
+                      transformStyle: 'preserve-3d',
+                    }}
+                >
+                  {'?'}
+                </FrontBox>
+                <BackBox
+                    style={{
+                      transform: rotateX.to(val => `perspective(600px) rotateX(${180 - val}deg)`),
+                      transformStyle: 'preserve-3d',
+                    }}
+                >
+                  {items[i]}
+                </BackBox>
+              </Box>
+          ))}
+        </Container>
+      </AppContainer>
+  )
+}
